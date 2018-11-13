eae314c69beb5a03360fe8f3c2eeec56a587446e
Jerrek <Jerrek@mail.ru>
2018-11-13 14:45:41 +0300

    добавил мейн

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import com.selenium.test.webtestsbase.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by kschegolev on 12.02.2018.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("MAIN START");
+        System.out.println("MAIN END");
+    }
+};
